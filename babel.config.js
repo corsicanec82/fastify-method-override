7fa2aa62d335c7c2aaf8ca30c82c4eef79b11d34
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2020-02-01 18:16:36 +0200

    remove babel runtime

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env'],
-  ],
-  plugins: [
-    '@babel/plugin-transform-runtime',
+    ['@babel/preset-env', {
+      targets: {
+        node: 'current',
+      },
+    }],
   ],
 };
